0d3b2435f62583b05da74a7a7dda2525327ab4cb
Hristijana <hristijanas@gmail.com>
2019-10-28 15:24:29 +0100

    zelena pozadina

--- eco-app/src/components/herotext/HeroText.js
+++ eco-app/src/components/herotext/HeroText.js
@@ -4,7 +4,9 @@ function HeroText() {
   return (
     
     <div className="calculator">
-      <h1>herocalc</h1>
+      <h1>РЕЦИКЛИРАЊЕТО НЕ БИЛО НИКОГАШ ПОЛЕСНО
+		ПРОВЕРЕТЕ И ИНФОРМИРАЈТЕ СЕ КАКО ДА РЕЦИКЛИРАТЕ ОТПАД
+</h1>
     </div>
   );
 }
