131013f7473763e17815bfa9fd0e6a7fbe38c2ee
Hristijana <hristijanas@gmail.com>
2019-10-29 12:32:33 +0100

    stil vo kontakt

--- eco-app/src/components/mapi/Mapi.js
+++ eco-app/src/components/mapi/Mapi.js
@@ -7,9 +7,9 @@ function Mapi() {
     return(
         <div className='mapi'>
             <Map center={[42.0050, 21.4408]} zoom={12} width={600} height={400}>
-      <Marker anchor={[50.874, 4.6947]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+      <Marker anchor={[42.0050, 21.4408]} payload={1} onClick={({ event, anchor, payload }) => {}} />
    
-      <Overlay anchor={[50.879, 4.6997]} offset={[120, 79]}>
+      <Overlay anchor={[42.0050, 21.4408]} offset={[120, 79]}>
         <img src='pigeon.jpg' width={240} height={158} alt='' />
       </Overlay>
     </Map>
