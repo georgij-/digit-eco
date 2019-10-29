8415256cb2759d550133ecc6df4092dbbd5282ae
Hristijana <hristijanas@gmail.com>
2019-10-29 15:05:12 +0100

    neshto neshto

--- eco-app/src/components/mapi/Mapi.js
+++ eco-app/src/components/mapi/Mapi.js
@@ -7,7 +7,13 @@ function Mapi() {
     return(
         <div className='mapi'>
             <Map center={[42.0050, 21.4408]} zoom={13} height={600} metaWheelZoom={true}>
-      <Marker anchor={[42.0050, 21.4408]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+      <Marker anchor={[41.98854, 21.44999]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+      <Marker anchor={[41.99261, 21.49275]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+      <Marker anchor={[41.95338, 21.49002]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+      <Marker anchor={[41.99014, 21.49170]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+      <Marker anchor={[42.00712, 21.37231]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+      <Marker anchor={[41.96595, 21.47049]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+      <Marker anchor={[41.96436, 21.45593]} payload={1} onClick={({ event, anchor, payload }) => {}} />
    
       <Overlay anchor={[42.0050, 21.4408]} offset={[120, 79]}>
         <img src='pigeon.jpg' width={240} height={158} alt='' />
