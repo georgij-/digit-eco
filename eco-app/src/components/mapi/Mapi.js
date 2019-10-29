88bf8e0954cab22fc91aaa688b245b64e6bae77f
Hristijana <hristijanas@gmail.com>
2019-10-29 11:57:33 +0100

    stavena e mapata bez stiliranje

--- eco-app/src/components/mapi/Mapi.js
+++ eco-app/src/components/mapi/Mapi.js
@@ -1,9 +1,18 @@
 import React from 'react';
+import Map from 'pigeon-maps';
+import Marker from 'pigeon-marker';
+import Overlay from 'pigeon-overlay';
 
 function Mapi() {
     return(
         <div className='mapi'>
-            <h2>Tuka e naslovot za mapa</h2>
+            <Map center={[42.0050, 21.4408]} zoom={12} width={600} height={400}>
+      <Marker anchor={[50.874, 4.6947]} payload={1} onClick={({ event, anchor, payload }) => {}} />
+   
+      <Overlay anchor={[50.879, 4.6997]} offset={[120, 79]}>
+        <img src='pigeon.jpg' width={240} height={158} alt='' />
+      </Overlay>
+    </Map>
         </div>
     );
 }
