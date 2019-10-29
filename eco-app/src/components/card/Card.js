a6055b03ad5bd2bddf36209d42218b93b994895e
Georgij Golaboski <georgij.golaboski@gmail.com>
2019-10-29 10:39:14 +0100

    stil na karticki

--- eco-app/src/components/card/Card.js
+++ eco-app/src/components/card/Card.js
@@ -3,10 +3,14 @@ import React from 'react';
 function Card() {
     return(
         <div className="card">
-            <h2>Karticka</h2>
+            <div className="card_header">
+            <h4>Karticka</h4>
+            </div>
+            <div className="card_body">
             <p>
                 Lorem ipsum
             </p>
+            </div>
         </div>
     );
 }
