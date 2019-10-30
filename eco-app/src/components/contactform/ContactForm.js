693f88b887d0a6fb811fac953f50c22642d643ce
Georgij Golaboski <georgij.golaboski@gmail.com>
2019-10-30 14:53:59 +0100

    final changes

---
+++ eco-app/src/components/contactform/ContactForm.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+function ContactForm() {
+    return(
+        <div id="ContactForm" className="container">
+            <div id="form">
+            <h4>Контакт</h4>
+            <p>Пополнете ја оваа контакт форма ако имате дополнителни прашања околу рециклирањето.</p>
+            </div>
+        </div>
+    );
+}
+
+export default ContactForm;
