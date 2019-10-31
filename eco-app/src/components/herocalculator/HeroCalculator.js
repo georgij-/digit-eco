eab93778fe0968910e32771ba640d8503cef5242
Georgij Golaboski <georgij.golaboski@gmail.com>
2019-10-31 14:50:55 +0100

    test

--- eco-app/src/components/herocalculator/HeroCalculator.js
+++ eco-app/src/components/herocalculator/HeroCalculator.js
@@ -1,39 +1,25 @@
 import React, { useState } from 'react';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
-import RadioButtons from './RadioButtons';
+// import RadioButtons from './RadioButtons';
 
-class HeroCalculator extends React.Component {
+function HeroCalculator() {
+  const [input, setInput] = useState(''); // '' is the initial state value
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      inputValue: ''
-    };
-  }
-
-  render() {
     return (
       <div className="calculator">
         <h1>Информативна пресметка</h1>
         <p>Пресметајте колку суров материјал за рециклирање е потребен за да се добие еден од следниве прозиводи:</p>
-        <input type="text" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} />
-          <br />        <br />
-          <RadioButtons />
-          <h1>Ќе добиете</h1>
-  
-          <span className="calculatorResult"></span>
+        <input type="text" onChange={(evt) => { console.log(evt.target.value); }} />
+        <span class="radio-btn"><input value={input} onInput={e => setInput(e.target.value)} type="radio"/>Пластика</span>
+        <span class="radio-btn"><input type="radio" />Хартија</span>
+        <span class="radio-btn"><input type="radio" />Стакло</span>
+
+        <h1>Ќе добиете</h1>
+         <span className="calculatorResult"></span>
       </div>
     );
-  }
-  updateInputValue(evt) {
-    this.setState({
-      inputValue: evt.target.value
-    });
-  }
-
 }
-
 
 
 export default HeroCalculator;
