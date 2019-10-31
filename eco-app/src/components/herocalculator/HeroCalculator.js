90db35b2dfdac17a25141d7717e36133f813189d
Georgij Golaboski <georgij.golaboski@gmail.com>
2019-10-31 09:53:57 +0100

    pred sindi

--- eco-app/src/components/herocalculator/HeroCalculator.js
+++ eco-app/src/components/herocalculator/HeroCalculator.js
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import InputAdornment from '@material-ui/core/InputAdornment';
 import TextField from '@material-ui/core/TextField';
 import RadioButtons from './RadioButtons';
-function HeroCalculator() {
-  return (
-    <div className="calculator">
-      <h1>Информативна пресметка</h1>
-      <p>Пресметајте колку суров материјал за рециклирање е потребен за да се добие еден од следниве прозиводи:</p>
-      <TextField
-          id="standard-start-adornment"
-          InputProps={{
-            startAdornment: <InputAdornment position="start">Kg</InputAdornment>,
-          }}
-        />
-        <br />        <br />
-        <RadioButtons />
-        <h1>Ќе добиете</h1>
-        <span className="calculatorResult"></span>
-    </div>
-  );
+
+class HeroCalculator extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      inputValue: ''
+    };
+  }
+
+  render() {
+    return (
+      <div className="calculator">
+        <h1>Информативна пресметка</h1>
+        <p>Пресметајте колку суров материјал за рециклирање е потребен за да се добие еден од следниве прозиводи:</p>
+        <input type="text" value={this.state.inputValue} onChange={evt => this.updateInputValue(evt)} />
+          <br />        <br />
+          <RadioButtons />
+          <h1>Ќе добиете</h1>
+  
+          <span className="calculatorResult"></span>
+      </div>
+    );
+  }
+  updateInputValue(evt) {
+    this.setState({
+      inputValue: evt.target.value
+    });
+  }
+
 }
+
+
 
 export default HeroCalculator;
