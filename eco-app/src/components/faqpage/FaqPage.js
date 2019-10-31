24fff21e216fba6998a36db50096629d3d24ed32
Georgij Golaboski <georgij.golaboski@gmail.com>
2019-10-31 11:06:56 +0100

    faq

--- eco-app/src/components/faqpage/FaqPage.js
+++ eco-app/src/components/faqpage/FaqPage.js
@@ -60,37 +60,75 @@ export default function FaqPanel() {
       <Zoom>
       <ExpansionPanel square expanded={expanded === 'panel1'} onChange={handleChange('panel1')}>
         <ExpansionPanelSummary aria-controls="panel1d-content" id="panel1d-header">
-          <Typography>Collapsible Group Item #1</Typography>
+          <Typography>Дали рециклирањето е најдобрата опција? Кои други опции постојат?</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Најефективниот начин да се намали отпадот и најдобрата стратегија за зачувување на природата е да не се прави ѓубре воопшто. Редукција на изворот на ѓубрето, како и реискористувањето на материјалите се најфункционалните начини да се зачуваат природните ресурси, да се заштити околината и да се заштедат пари.
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
       <ExpansionPanel square expanded={expanded === 'panel2'} onChange={handleChange('panel2')}>
         <ExpansionPanelSummary aria-controls="panel2d-content" id="panel2d-header">
-          <Typography>Collapsible Group Item #2</Typography>
+          <Typography>Како да ги дознаам моите локални опции за рециклирање</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Ве молиме контактирајте ја вашата локална општина или заедница за да ги дознаете вашите опции.
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
       <ExpansionPanel square expanded={expanded === 'panel3'} onChange={handleChange('panel3')}>
         <ExpansionPanelSummary aria-controls="panel3d-content" id="panel3d-header">
-          <Typography>Collapsible Group Item #3</Typography>
+          <Typography>Зошто е важно да ставаме само предмети кои можат да се рециклираат во нашата отпадна корпа?</Typography>
         </ExpansionPanelSummary>
         <ExpansionPanelDetails>
           <Typography>
-            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex,
-            sit amet blandit leo lobortis eget. Lorem ipsum dolor sit amet, consectetur adipiscing
-            elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.
+          Ставање на предмети во отпадната корпа кои не можат да се рециклираат можат да го контаминираат редот на рецкилирање. Одкако овие предмети ќе пристигнат во рециклирачките центри, можат да направат штета на опремата, машините и слично. Истотака одкако ќе пристигнат, тие додатно мора да се сортираат и да се пратат на отпад - што дополнително ги зголемува оперативните трошоци на центарот. Затоа е важно да проверите што фрлате :)
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel square expanded={expanded === 'panel4'} onChange={handleChange('panel4')}>
+        <ExpansionPanelSummary aria-controls="panel4d-content" id="panel4d-header">
+          <Typography>
+Зошто некои работи кои изгледаат како да можат да се рециклираат - не се прифатени од рециклирачкиот центар?</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          Вашиот локален рециклирачки центар можеби нема да ги прима сите рециклирачки предмети. Ова е посебно точно со пластика. Додека пластичните шишиња се нај рециклираниот предмет, други пластики може да не бидат прифатени во вашиот центар, затоа информирајте се за тоа кој видови предмети ќе бидат прифатени.
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel square expanded={expanded === 'panel5'} onChange={handleChange('panel5')}>
+        <ExpansionPanelSummary aria-controls="panel5d-content" id="panel5d-header">
+          <Typography>
+Што не смеам да ставам во мојата корпа за рециклирање?</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          
+Никако не смеете да фрлате:
+-Игли
+-Гумени црева за домаќинство
+-Боци за пропан (плин) / Празни алуминиумски боци
+-Шприцеви, скршено стакло, сијалици
+          </Typography>
+        </ExpansionPanelDetails>
+      </ExpansionPanel>
+      <ExpansionPanel square expanded={expanded === 'panel6'} onChange={handleChange('panel6')}>
+        <ExpansionPanelSummary aria-controls="panel6d-content" id="panel6d-header">
+          <Typography>Кои се најчести предмети кои можам да ги ставам во мојата корпа за рециклирање?</Typography>
+        </ExpansionPanelSummary>
+        <ExpansionPanelDetails>
+          <Typography>
+          -Картон/Хартија
+-Кутии за храна
+-Пошта
+-Алуминиумски лименки
+-Конзерви
+-Стаклени шишиња
+-Тегли
+-Пластични шишиња и капачиња
           </Typography>
         </ExpansionPanelDetails>
       </ExpansionPanel>
